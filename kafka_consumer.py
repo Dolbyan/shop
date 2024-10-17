457d2e943eead4b191412471f4bb517237f026ac
Dolbyan <pepik767@wp.pl>
2024-10-17 22:52:14 +0200

    CICD

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,34 +1,26 @@
 import os
 import django
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "adminservice.eshop.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "kafka_consumer_settings"
 django.setup()
 from adminservice.app.utilities import KafkaService
 from adminservice.app.models import Items as AdminItems
 from ecomm.app.models import Items as UserItems
 from django.core.exceptions import ObjectDoesNotExist
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-logger.debug("Starting Django setup")
-django.setup()
-logger.debug("Django setup complete")
 
 
 class KafkaConsumerApp:
     def __init__(self):
         self.kafka_service = KafkaService()
 
-    def set_django_settings(self, source):
-
-        if source == "user":
-            os.environ['DJANGO_SETTINGS_MODULE'] = "adminservice.eshop.settings"
-        elif source == "admin":
-            os.environ['DJANGO_SETTINGS_MODULE'] = "ecomm.ecomm.settings"
-
-        django.setup()
+    # def set_django_settings(self, source):
+    #
+    #     if source == "user":
+    #         os.environ['DJANGO_SETTINGS_MODULE'] = "adminservice.eshop.settings"
+    #     elif source == "admin":
+    #         os.environ['DJANGO_SETTINGS_MODULE'] = "ecomm.ecomm.settings"
+    #
+    #     django.setup()
 
     def process_message(self, message):
 
@@ -37,11 +29,10 @@ class KafkaConsumerApp:
         data = message.get('data')
         source = message.get("source")
 
-        self.set_django_settings(source)
+        # self.set_django_settings(source)
 
         if source == "user":
             self.update_admin_inventory(data)
-
         elif source == "admin" and model == 'Items':
             if operation == 'create':
                 self.handle_create(data)
@@ -52,14 +43,14 @@ class KafkaConsumerApp:
 
     def update_admin_inventory(self, data):
         try:
-            item = AdminItems.objects.get(id=data.get("id"))
+            item = AdminItems.objects.using('admin_db').get(id=data.get("id"))
             item.amount -= int(data.get('amount'))
             item.save()
         except ObjectDoesNotExist:
             print(f"Item with id {data.get('id')} does not exist.")
 
     def handle_create(self, data):
-        item = UserItems.objects.create(
+        item = UserItems.objects.using('ecomm_db').create(
             name=data.get('name'),
             type=data.get('category'),
             description=data.get('description'),
@@ -72,7 +63,7 @@ class KafkaConsumerApp:
 
     def handle_update(self, data):
         try:
-            item = UserItems.objects.get(id=data.get('id'))
+            item = UserItems.objects.using('ecomm_db').get(id=data.get('id'))
             item.name = data.get('name', item.name)
             item.type = data.get('category', item.type)
             item.description = data.get('description', item.description)
@@ -86,7 +77,7 @@ class KafkaConsumerApp:
 
     def handle_delete(self, data):
         try:
-            item = UserItems.objects.get(id=data.get('id'))
+            item = UserItems.objects.using('ecomm_db').get(id=data.get('id'))
             item.delete()
         except ObjectDoesNotExist:
             print(f"Item with id {data.get('id')} does not exist.")
