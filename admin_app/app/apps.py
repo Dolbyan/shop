3c241124e4058b8286d51f3c7dd0e2cd99f7d3d6
Dolbyan <pepik767@wp.pl>
2024-11-20 23:09:50 +0100

    cicd

--- admin_app/app/apps.py
+++ admin_app/app/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AdminServiceAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_app.app'
-    label = 'admin_service'
+    label = 'app'
--- admin_app/eshop/settings.py
+++ admin_app/eshop/settings.py
@@ -78,7 +78,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-    'admin_app.app',
+    'admin_app.app.apps.AdminServiceAppConfig',
     'storages',
     'eshop',
 ]
