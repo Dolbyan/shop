6133b77efcac1c0a6279cb6ad5b55343ef4e36b1
Dolbyan <pepik767@wp.pl>
2024-12-18 20:43:37 +0100

    cicd

--- admin_app/eshop/urls.py
+++ admin_app/eshop/urls.py
@@ -19,12 +19,14 @@ from django.urls import path, include, re_path
 from django.conf import settings
 import os
 from django.conf.urls.static import static
-
+from gunicorn.app.pasterapp import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("admin_app.app.urls")),
+    path('favicon.ico', serve, {'document_root': os.path.join(settings.STATIC_ROOT, 'favicon.ico')}),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static('favicon.ico', document_root=os.path.join(settings.BASE_DIR, 'admin_app/static'))
