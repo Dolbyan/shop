78b75e19a5df7b879922757a592fa09dfb49bf78
Dolbyan <pepik767@wp.pl>
2025-01-25 21:01:30 +0100

    cicd

--- admin_app/eshop/settings.py
+++ admin_app/eshop/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'bikeshop'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '5434'),
     }
 }
 # DATABASES = {
